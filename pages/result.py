9561d20cf08c49bfa87cd709131e344e3fc392a4
Kelby Styler <kelbystyler@gmail.com>
2022-03-22 13:09:43 -0700

    Attempts to figure out why tests are failing Seems to be a race condition. Copied tutorial code directly to check and that seems to be the case.

--- pages/result.py
+++ pages/result.py
@@ -1,4 +1,3 @@
-from turtle import title
 from playwright.sync_api import Page
 from typing import List
 
@@ -8,11 +7,11 @@ class DuckDuckGoResultPage:
         self.page = page
         self.result_link = page.locator('.result__title a.result__a')
         self.search_input = page.locator('#search_form_input')
-
+    
     def result_link_titles(self) -> List[str]:
         self.result_link.nth(4).wait_for()
         return self.result_link.all_text_contents()
-
+    
     def result_link_titles_contain_phrase(self, phrase: str, minimum: int = 1) -> bool:
         titles = self.result_link_titles()
         matches = [t for t in titles if phrase.lower() in t.lower()]
--- tests/test_search.py
+++ tests/test_search.py
@@ -1,23 +1,46 @@
+from multiprocessing.connection import wait
+import pytest
+
 from pages.result import DuckDuckGoResultPage
 from pages.search import DuckDuckGoSearchPage
 from playwright.sync_api import expect, Page
 
+
+ANIMALS = [
+    'panda',
+    'python',
+    'polar bear',
+    # 'parrot',
+    # 'porcupine',
+    # 'parakeet',
+    # 'pangolin',
+    # 'panther',
+    # 'platypus',
+    # 'peacock'
+]
+
+# There is a race condition when using headless w/ multiple
+# cores, my guess is that I need to add some type of waiting
+# when the results appear. Should be a good exercise
+
+@pytest.mark.parametrize('phrase', ANIMALS)
 def test_basic_duckduckgo_search(
+    phrase: str,
     page: Page,
     search_page: DuckDuckGoSearchPage,
     result_page: DuckDuckGoResultPage) -> None:
-
+    
     # Given the DuckDuckGo home page is displayed
     search_page.load()
 
     # When the user searches for a phrase
-    search_page.search('panda')
+    search_page.search(phrase)
 
     # Then the search result query is the phrase
-    expect(result_page.search_input).to_have_value('panda')
+    expect(result_page.search_input).to_have_value(phrase)
 
     # And the search result links pertain to the phrase
-    assert result_page.result_link_titles_contain_phrase('panda')
+    assert result_page.result_link_titles_contain_phrase(phrase)
 
     # And the search result title contains the phrase
-    expect(page).to_have_title('panda at DuckDuckGo')
+    expect(page).to_have_title(f'{phrase} at DuckDuckGo')
